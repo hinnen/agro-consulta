2a56e025aef9380efa50f4daf80afba541ab07ee
hinnen <renanhinnen@gmail.com>
2026-03-15 20:23:14 -0300

    1.2

--- produtos/urls.py
+++ produtos/urls.py
@@ -4,3 +4,9 @@ from .views import consulta_produtos
 urlpatterns = [
     path('consulta/', consulta_produtos, name='consulta_produtos'),
 ]
+
+from .views import api_buscar_produtos
+
+urlpatterns += [
+    path("api/buscar/", api_buscar_produtos),
+]
