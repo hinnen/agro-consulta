2bb2c2e8070f9fd0ded70fea4ee1688c1a521888
hinnen <renanhinnen@gmail.com>
2026-03-18 17:29:53 -0300

    11

--- produtos/urls.py
+++ produtos/urls.py
@@ -4,10 +4,12 @@ from .views import (
     consulta_produtos,
     api_buscar_produtos,
     api_sugestoes_produtos,
+    api_ajustar_estoque,
 )
 
 urlpatterns = [
     path("consulta/", consulta_produtos, name="consulta_produtos"),
     path("api/buscar/", api_buscar_produtos, name="api_buscar_produtos"),
     path("api/sugestoes/", api_sugestoes_produtos, name="api_sugestoes_produtos"),
+    path("api/ajustar/", api_ajustar_estoque, name="api_ajustar_estoque"),
 ]
