80d9bb4dede37c189598dd46239f6ca7dac8d976
hinnen <renanhinnen@gmail.com>
2026-03-17 23:31:22 -0300

    Consulta estoque funcionando + base multiempresa iniciada

--- produtos/models.py
+++ produtos/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 
 class Produto(models.Model):
-    codigo_interno = models.CharField(max_length=50, unique=True)
+    empresa = models.ForeignKey(
+        'base.Empresa',
+        on_delete=models.CASCADE,
+        related_name='produtos',
+        verbose_name='Empresa',
+        null=True,
+        blank=True,
+    )
+    codigo_interno = models.CharField(max_length=50)
     codigo_barras = models.CharField(max_length=50, blank=True, null=True)
     nome = models.CharField(max_length=200)
     categoria = models.CharField(max_length=100, blank=True, null=True)
@@ -18,6 +26,12 @@ class Produto(models.Model):
         verbose_name = 'Produto'
         verbose_name_plural = 'Produtos'
         ordering = ['nome']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['empresa', 'codigo_interno'],
+                name='unique_codigo_interno_por_empresa',
+            )
+        ]
 
     def __str__(self):
-        return f'{self.codigo_interno} - {self.nome}'
+        return f'{self.codigo_interno} - {self.nome}'   
