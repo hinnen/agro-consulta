4fc2b98477eb60a3f54022b1ede6f6dd43a2e6fd
hinnen <renanhinnen@gmail.com>
2026-04-01 23:25:35 -0300

    scxvxvcx

--- financeiro/api/serializers.py
+++ financeiro/api/serializers.py
@@ -8,6 +8,23 @@ class ResumoOperacionalQuerySerializer(serializers.Serializer):
     data_fim = serializers.DateField(required=True)
     modo = serializers.ChoiceField(choices=["empresa", "grupo"], required=True)
     dias_periodo = serializers.IntegerField(required=False, default=30, min_value=1)
+    fonte = serializers.ChoiceField(
+        choices=["postgres", "mongo"],
+        default="postgres",
+        required=False,
+    )
+    por = serializers.ChoiceField(
+        choices=["competencia", "vencimento", "pagamento"],
+        default="competencia",
+        required=False,
+    )
+    valor = serializers.ChoiceField(
+        choices=["bruto", "realizado"],
+        default="bruto",
+        required=False,
+    )
+    contas = serializers.CharField(required=False, allow_blank=True, default="")
+    incluir_linhas = serializers.BooleanField(required=False, default=False)
 
     def validate(self, attrs):
         modo = attrs["modo"]
