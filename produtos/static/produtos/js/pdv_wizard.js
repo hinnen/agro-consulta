a5f8d6048b68e2ea5972ab7731ede1edef8155cd
Renan Hinnen <RenanHinnen@users.noreply.github.com>
2026-06-29 03:30:56 -0300

    Popups entrega com tipografia maior para leitura na loja.
    Titulo, total troco, labels e botoes escalam com clamp/rem no padrao Agro Display Scale.

--- produtos/static/produtos/js/pdv_wizard.js
+++ produtos/static/produtos/js/pdv_wizard.js
@@ -4099,7 +4099,7 @@
         }
         if (header) {
             header.className =
-                'rounded-t-[1.05rem] border-b border-white/15 px-4 py-2.5 sm:px-5 sm:py-3 ' +
+                'pdv-ed-header rounded-t-[1.05rem] border-b border-white/15 ' +
                 t.header;
         }
         if (body) {
